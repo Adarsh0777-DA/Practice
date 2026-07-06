e6663e70695be630421078979d105f3e5a2c6d47
Adarsh Choubey <adarsh0choubey@gmail.com>
2026-07-06 17:04:25 +0530

    NEW FILE

---
+++ Codder.py
@@ -0,0 +1,12 @@
+employees = [
+    {"name": "Amit", "salary": 50000},
+    {"name": "Riya", "salary": 65000},
+    {"name": "Rahul", "salary": 45000},
+    {"name": "Priya", "salary": 70000}
+]
+
+# Find:
+# 1. Employee with highest salary
+# 2. Average salary
+# 3. Employees earning more than 60000
+# 4. Sort employees by salary
